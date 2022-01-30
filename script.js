d5ea654910fea9d5cbb50ea84582a2a30f311598
TechTalkZach <91166606+TechTalkZach@users.noreply.github.com>
2022-01-30 15:10:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 var todayDate = new Date();
 var currentMois = todayDate.getMonth();
 
-function geCurrentMonth() {
+function getCurrentMonth() {
 
     var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Décembre"];
     var todayDate = new Date();
     var currentMois = todayDate.getMonth();
+
+    return currentMois;
+
+ } 
+
+ function convertMonthToString() {
+
+    var mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Décembre"];
+
     var nomDuMois = mois[currentMois];
 
     return nomDuMois;
 
-}
-console.log(getCurrentMonth());
+ }
+
+ function getSeason() {
+
+    var season = ["Winter", "Spring", "Summer", "Fall"];
+
+    
+
+    if(currentMois == 11 || currentMois == 0 || currentMois == 1) 
+
+        return season[0];
+
+        else if(currentMois == 2 || currentMois == 3 || currentMois == 4)
+
+        return season[1];
+
+        else if(currentMois == 5 || currentMois == 6 || currentMois == 7)
+
+        return season[2];
+
+        else 
+        return season[3];
+ }
+
+ function getRandomTemperature() {
+
+
+ }
+
+ let numericMonth = getCurrentMonth();
+ console.log(numericMonth);
+ console.log(convertMonthToString(numericMonth));
+ console.log(getSeason(numericMonth));
+
